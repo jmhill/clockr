f0db146e5c93c0caacfd95815aaafee803417a92
Justin Hill <justin@jmhill.dev>
2024-09-22 00:05:18 -0400

    test basic auth middleware

--- main.tsx
+++ main.tsx
@@ -1,14 +1,26 @@
 import * as hono from "@hono/hono";
+import { basicAuth } from "@hono/hono/basic-auth";
 
 const app = new hono.Hono();
 
-app.get("/", (c) => c.html(
-  <html>
-    <body>
-      <h1>Hello JSX</h1>
-      <p>This is just a test to make sure that hono jsx rendering works as intended.</p>
-    </body>
-  </html>  
-));
+app.get(
+  "/",
+  basicAuth({
+    username: "demo",
+    password: "demo",
+  }),
+  (c) =>
+    c.html(
+      <html>
+        <body>
+          <h1>Hello JSX</h1>
+          <p>
+            This is just a test to make sure that hono jsx rendering works as
+            intended.
+          </p>
+        </body>
+      </html>,
+    ),
+);
 
 Deno.serve(app.fetch);
